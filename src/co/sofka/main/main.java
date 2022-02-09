d32c6efd3981887a738492de76ce839a6b9944d4
francheskito <franccesco.giordano11@gmail.com>
2022-02-09 19:05:21 -0300

    listo para trabajar

--- src/co/sofka/main/main.java
+++ src/co/sofka/main/main.java
@@ -1,7 +1,60 @@
 package co.sofka.main;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class main {
+    public static  Integer[] memoria = new Integer[43];
     public static void main(String[] args) {
-                
+        System.out.println("Buenas tardes creador, ingrese las tareas a realiza, al terminar ingrese 0");
+        Scanner sc = new Scanner(System.in);
+        String instruccion = sc.nextLine();
+        int opcion = -1;
+        //  ArrayList instrucciones=new ArrayList<String>();
+
     }
+
+    public static void hacerTareas(String[] instrucciones) {
+        String[] instrucionesDesglosadas= new String[instrucciones.length];
+       String[] instruccionArelizar= new String[2];
+        String[] parametros= new String[2];
+        int i=0;
+        String instruccionx;
+        for (String instruccion: instrucciones) {
+
+            instruccionArelizar=instruccion.split(" ");
+            instruccionx=instruccionArelizar[0];
+            parametros=instruccionArelizar[1].split(",");
+        switch (instruccionx){
+            case "MOV":
+
+                break;
+            case "ADD":
+
+                break;
+            case "DEC":
+
+               break;
+            case "INC":
+
+                break;
+            case "JMP":
+
+                break;
+
+            case "JZ":
+
+                break;
+
+
+
+
+        }
+            i++;
+        }
+    }
+    public static void MOV (String parametro1, String parametro2){
+
+    }
+
 }
