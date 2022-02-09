3c5f2d399a20a24e0994010a0d7c5cc2171a82e3
Dehyro <mendezdehyro@gmail.com>
2022-02-09 18:56:56 -0500

    Corrección al método MOV

--- src/co/sofka/main/MOV.java
+++ src/co/sofka/main/MOV.java
@@ -2,11 +2,11 @@ package co.sofka.main;
 
 
 public class MOV {
-    //public static  Integer[] memoria = new Integer[43];
-    public static void mover(String parametro1, String parametro2){
 
-        int registro1 = Integer.parseInt(parametro1.substring(1,3));
-        int registro2 = Integer.parseInt(parametro2.substring(1,3));
+    public static void mover(String parametros[]){
+
+        int registro1 = Integer.parseInt(parametros[0].substring(1,3));
+        int registro2 = Integer.parseInt(parametros[1].substring(1,3));
         main.memoria[registro2] = main.memoria[registro1];
         //System.out.println(main.memoria[registro2]);
     }
